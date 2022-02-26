d89ae947a0baadef33342b171f2a02ffa92a3440
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-02-26 20:24:06 +0900

    🎨 - Changed from Utc to Local.

--- src/database/models/upcoming_object.rs
+++ src/database/models/upcoming_object.rs
@@ -1,20 +1,20 @@
 use core::option::Option;
 use std::str::FromStr;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Local};
 use crate::database::models::update_signature::UpdateSignature;
 
 use super::id_object::{ChannelId, VideoId};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
 pub struct Lives {
     video_id: VideoId,
     channel_id: Option<ChannelId>,
     title: String,
     description: String,
-    published_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-    will_start_at: Option<DateTime<Utc>>,
-    started_at: Option<DateTime<Utc>>,
+    published_at: DateTime<Local>,
+    updated_at: DateTime<Local>,
+    will_start_at: Option<DateTime<Local>>,
+    started_at: Option<DateTime<Local>>,
     thumbnail_url: String,
     update_signature: UpdateSignature
 }
