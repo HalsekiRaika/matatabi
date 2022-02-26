cdd6edb16406911bd670ffbed2fcbefd4b41431f
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-02-26 20:57:21 +0900

    🚧 - Implement the constructor in Lives.

--- src/database/models/upcoming_object.rs
+++ src/database/models/upcoming_object.rs
@@ -18,3 +18,53 @@ pub struct Lives {
     thumbnail_url: String,
     update_signature: UpdateSignature
 }
+
+pub struct LivesBuilder {
+    pub video_id: VideoId,
+    pub channel_id: Option<ChannelId>,
+    pub title: String,
+    pub description: String,
+    pub published_at: DateTime<Local>,
+    pub updated_at: DateTime<Local>,
+    pub will_start_at: Option<DateTime<Local>>,
+    pub started_at: Option<DateTime<Local>>,
+    pub thumbnail_url: String,
+    pub update_signature: UpdateSignature,
+    #[doc(hidden)]
+    pub init: ()
+}
+
+impl Default for LivesBuilder {
+    fn default() -> Self {
+        Self {
+            video_id: VideoId("none".to_string()),
+            channel_id: None,
+            title: "none".to_string(),
+            description: "none".to_string(),
+            published_at: Local::now(),
+            updated_at: Local::now(),
+            will_start_at: None,
+            started_at: None,
+            thumbnail_url: "none".to_string(),
+            update_signature: UpdateSignature::default(),
+            init: ()
+        }
+    }
+}
+
+impl LivesBuilder {
+    pub fn build(self) -> Lives {
+        Lives {
+            video_id: self.video_id,
+            channel_id: self.channel_id,
+            title: self.title,
+            description: self.description,
+            published_at: self.published_at,
+            updated_at: self.updated_at,
+            will_start_at: self.will_start_at,
+            started_at: self.started_at,
+            thumbnail_url: self.thumbnail_url,
+            update_signature: self.update_signature
+        }
+    }
+}
