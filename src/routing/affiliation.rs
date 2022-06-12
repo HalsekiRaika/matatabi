23a67c8b8150f2915981a4e43f6901307cb0d488
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-06-12 14:16:29 +0900

    ✨ - Define routing in Axum to try it out.

---
+++ src/routing/affiliation.rs
@@ -0,0 +1,32 @@
+use axum::{Extension, Json};
+use axum::extract::Path;
+use axum::http::StatusCode;
+use sqlx::PgPool;
+use crate::models::affiliation::Affiliation;
+use crate::database::models::affiliation_object::{Affiliations as AffiliationObject};
+
+use super::ApiError;
+use super::ErrorResponse;
+
+pub async fn get_affiliations(
+    Extension(pool): Extension<PgPool>
+) -> Result<Json<Vec<Affiliation>>, ErrorResponse> {
+    let aff_all = AffiliationObject::fetch_all(&pool).await
+        .map_err(|e| ApiError::new(e).report(StatusCode::INTERNAL_SERVER_ERROR))?
+        .into_iter()
+        .map(Affiliation::from)
+        .collect();
+    Ok(Json(aff_all))
+}
+
+pub async fn get_affiliation_from_id(
+    Path(id): Path<u64>,
+    Extension(pool): Extension<PgPool>
+) -> Result<Json<Affiliation>, ErrorResponse> {
+    let aff = AffiliationObject::fetch_name_from_id(id as i64, &pool).await
+        .map_err(|e| ApiError::new(e).report(StatusCode::INTERNAL_SERVER_ERROR))?
+        .map(Affiliation::from)
+        .ok_or_else(|| ApiError::reason(format!("{} is not found.", id)).report(StatusCode::NOT_FOUND))?;
+    Ok(Json(aff))
+}
+
--- src/server/axum.rs
+++ src/server/axum.rs
@@ -7,12 +7,29 @@ use crate::routing;
 pub async fn run_webapi_server(connection_instance: Pool<Postgres>) {
     let app = Router::new()
         .route("/", get(routing::version))
+        .route("/affiliations", get(routing::affiliation::get_affiliations))
+        .route("/affiliations/:id", get(routing::affiliation::get_affiliation_from_id))
         .layer(axum::Extension(connection_instance));
 
     let bind_address = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", bind_address);
     axum::Server::bind(&bind_address)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(exit())
         .await
         .unwrap_or_else(|_| panic!("Cannot startup webapi server!"))
+}
+
+async fn exit() {
+    let user_interrupt = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install keyboard interrupt.")
+    };
+
+    tokio::select! {
+        _ = user_interrupt => {}
+    }
+
+    tracing::info!("interrupt signal received. shutdown.")
 }
