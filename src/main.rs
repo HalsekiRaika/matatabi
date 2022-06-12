c05599e15323c673ab2112c4f458a8d00cb76dbc
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-06-12 13:49:28 +0900

    🔥 - Removed simplicity logger.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use dotenv::dotenv;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use logger::Logger;
 
 mod database;
 mod models;
 mod server;
-#[deprecated]
-mod routes;
 mod routing;
-mod logger;
 
 #[tokio::main]
-//#[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
     tracing_subscriber::registry()
@@ -21,8 +16,7 @@ async fn main() -> Result<(), std::io::Error> {
         .init();
     //env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let logger = Logger::new(Some("Matatabi"));
-    logger.info("Cats are crazy about Matatabi. ฅ^•ω•^ฅ");
+    tracing::info!("Cats are crazy about Matatabi. ฅ^•ω•^ฅ");
 
     database::postgres_database::migration()
         .await
