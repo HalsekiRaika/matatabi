c8152cd2a9b28081db41f2a7cd1494c72d1ff34c
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-06-12 13:52:16 +0900

    🎨 - Trait definition using related types.

--- src/database/models/mod.rs
+++ src/database/models/mod.rs
@@ -30,3 +30,48 @@ pub trait Transactable<T> {
     async fn exists(&self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<bool, sqlx::Error>;
     async fn delete(&self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<i64, sqlx::Error>;
 }
+
+
+/// Trait used to mediate basic SQL Transactions.
+///
+/// Use the SQL statement "Returning *" to use the value of the result after the SQL is executed for the return value
+#[async_trait::async_trait]
+pub trait Transact {
+    type TransactItem;
+
+    /// Consume the value and insert it into the database.
+    ///
+    /// [Ok()]: `T` - Value returned by SQL statement "Returning *".
+    ///
+    /// [Err()] - Error in sqlx.
+    async fn insert(self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<Self::TransactItem, sqlx::Error>;
+
+    /// Consumes a value and deletes the corresponding data from the database.
+    ///
+    /// [Ok()]: `T` - Value returned by SQL statement "Returning *".
+    ///
+    /// [Err()] - Error in sqlx.
+    async fn delete(self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<Self::TransactItem, sqlx::Error>;
+
+    /// Consumes a value and updates the value in the database.
+    ///
+    /// [Ok()] `(T, T)` - Return tuple (old, new). Value returned by SQL statement "Returning *".
+    ///
+    /// [Err()] - Error in sqlx.
+    async fn update(self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<(Self::TransactItem, Self::TransactItem), sqlx::Error>;
+
+    /// Verify that the corresponding data exists in the database.
+    ///
+    /// Use
+    ///
+    /// ```sql
+    /// SELECT EXISTS(SELECT 1 FROM data WHERE id LIKE $1)
+    /// ```
+    ///
+    /// or otherwise return the Bool value from the database.
+    ///
+    /// [Ok()]: `bool` - Value returned by SQL statement "Returning *".
+    ///
+    /// [Err()] - Error in sqlx.
+    async fn exists(&self, transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>) -> Result<bool, sqlx::Error>;
+}
