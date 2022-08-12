7cc66cef442a38694d2edff12116bc5a998d498a
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-08-13 02:44:05 +0900

    ✨ - Changed tracing-subscriber setting to apply tracing-appender.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use dotenv::dotenv;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
+use tracing_subscriber::Layer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -10,11 +12,17 @@ mod routing;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     dotenv().ok();
+    let appender = tracing_appender::rolling::daily(std::path::Path::new("./logs/"), "debug.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(appender);
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(|_| "matatabi=debug".into())))
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer()
+            .with_filter(tracing_subscriber::EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(|_| "matatabi=debug".into())))
+            .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG))
+        .with(tracing_subscriber::fmt::Layer::default()
+            .with_writer(non_blocking_appender)
+            .with_ansi(false)
+            .with_filter(tracing_subscriber::filter::LevelFilter::DEBUG))
         .init();
-    //env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     tracing::info!("Cats are crazy about Matatabi. ฅ^•ω•^ฅ");
 
