c578e96c7ad606bc382c36525020b887250c5646
ReiRokusanami <reirokusanami.rdh@gmail.com>
2022-08-24 04:38:01 +0900

    ✨ - Implemented data fetch on gRPC server.

--- src/server/salmon.rs
+++ src/server/salmon.rs
@@ -1,17 +1,19 @@
 use std::collections::vec_deque::VecDeque;
 use std::fmt::Display;
 use std::net::ToSocketAddrs;
+use std::pin::Pin;
 use chrono::{DateTime, Local, TimeZone};
-use futures::StreamExt;
 
 use sqlx::Postgres;
+use tokio::sync::mpsc;
 use tokio::time::Instant;
+use tokio_stream::wrappers::ReceiverStream;
 use tonic::transport::Server;
 use tonic::{Request, Response, Status, Streaming};
 use proto::salmon_api_server::{SalmonApiServer, SalmonApi};
-use proto::{Affiliation, Channel, Liver, Live, TaskResult};
+use proto::{Affiliation, Channel, Liver, Video, TaskResult, Void};
 
-use crate::database::models::Accessor;
+use crate::database::models::{Accessor, Fetch};
 use crate::database::models::affiliation_object::AffiliationObject;
 use crate::database::models::livers_object::LiverObject;
 use crate::database::models::channel_object::{ChannelObject, ChannelObjectBuilder};
@@ -33,6 +35,7 @@ impl SalmonAutoCollector {
 }
 
 type SalmonResult<T> = Result<Response<T>, Status>;
+type SalmonResponseStream<T> = Pin<Box<dyn futures::Stream<Item = Result<T, Status>> + Send>>;
 
 #[tonic::async_trait]
 impl SalmonApi for SalmonAutoCollector {
@@ -51,6 +54,26 @@ impl SalmonApi for SalmonAutoCollector {
     async fn insert_affiliation(&self, req: Request<Streaming<Affiliation>>) -> SalmonResult<TaskResult> {
         self.collect::<Affiliation, AffiliationObject>(req).await
     }
+
+    type FetchAllVideosStream = SalmonResponseStream<Video>;
+    async fn fetch_all_videos(&self, _: Request<Void>) -> SalmonResult<Self::FetchAllVideosStream> {
+        self.fetch::<VideoObject, Video>().await
+    }
+
+    type FetchAllChannelsStream = SalmonResponseStream<Channel>;
+    async fn fetch_all_channels(&self, _: Request<Void>) -> SalmonResult<Self::FetchAllChannelsStream> {
+        self.fetch::<ChannelObject, Channel>().await
+    }
+
+    type FetchAllLiversStream = SalmonResponseStream<Liver>;
+    async fn fetch_all_livers(&self, _: Request<Void>) -> SalmonResult<Self::FetchAllLiversStream> {
+        self.fetch::<LiverObject, Liver>().await
+    }
+
+    type FetchAllAffiliationsStream = SalmonResponseStream<Affiliation>;
+    async fn fetch_all_affiliations(&self, _: Request<Void>) -> SalmonResult<Self::FetchAllAffiliationsStream> {
+        self.fetch::<AffiliationObject, Affiliation>().await
+    }
 }
 
 impl SalmonAutoCollector {
@@ -58,6 +81,7 @@ impl SalmonAutoCollector {
         where T: From<R> + Display + Accessor<Item = T>,
               R: DeleteFlag
     {
+        use futures::StreamExt;
         let dur_now = Instant::now();
         let collector_item = receive.into_inner()
             .map(Result::unwrap)    
@@ -102,6 +126,34 @@ impl SalmonAutoCollector {
         tracing::info!("transaction elapsed {}ms", dur_now.elapsed().as_millis());
         Ok(Response::new(TaskResult { message: "".to_string() }))
     }
+
+    pub async fn fetch<D, G>(&self) -> SalmonResult<SalmonResponseStream<G>>
+        where D: From<G> + Send + 'static + Display + Fetch<Item = D>,
+              G: From<D> + Send + 'static
+    {
+        use tokio_stream::StreamExt;
+        let mut transaction = self.pool.begin().await
+            .map_err(|e| Status::failed_precondition(format!("Failed to begin build transaction: {:?}", e)))?;
+        let db_item = D::fetch_all(&mut transaction).await
+            .map_err(|e| Status::internal(format!("Failed fetch: {:?}", e)))?
+            .into_iter()
+            .map(G::from)
+            .collect::<Vec<_>>();
+        let mut stream = Box::pin(tokio_stream::iter(db_item).throttle(std::time::Duration::from_millis(20)));
+
+        let (tx, rx) = mpsc::channel(128);
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {},
+                    Err(_) => { break; }
+                }
+            }
+        });
+
+        let res_stream = ReceiverStream::new(rx);
+        Ok(Response::new(Box::pin(res_stream) as SalmonResponseStream<G>))
+    }
 }
 
 impl From<Affiliation> for AffiliationObject {
@@ -110,9 +162,31 @@ impl From<Affiliation> for AffiliationObject {
     }
 }
 
+impl From<AffiliationObject> for Affiliation {
+    fn from(obj: AffiliationObject) -> Self {
+        Self { 
+            affiliation_id: obj.affiliation_id().into(),
+            name: obj.name().to_owned(),
+            delete: false
+        }
+    }
+}
+
 impl From<Liver> for LiverObject {
     fn from(data: Liver) -> Self {
         LiverObject::new(data.liver_id, data.affiliation_id, data.name, data.localized_name)
+    }
+}
+
+impl From<LiverObject> for Liver {
+    fn from(obj: LiverObject) -> Self {
+        Self {
+            liver_id: obj.liver_id().into(),
+            name: obj.name().to_owned(),
+            localized_name: obj.localized_name().to_owned(),
+            affiliation_id: obj.affiliation_id().map(Into::into),
+            delete: false
+        }
     }
 }
 
@@ -131,8 +205,21 @@ impl From<Channel> for ChannelObject {
     }
 }
 
-impl From<Live> for VideoObject {
-    fn from(data: Live) -> Self {
+impl From<ChannelObject> for Channel {
+    fn from(obj: ChannelObject) -> Self {
+        Self {
+            channel_id: obj.channel_id().to_owned().into(),
+            liver_id: obj.liver_id().map(Into::into),
+            logo_url: obj.logo_url().to_owned(),
+            published_at: Some(::prost_types::Timestamp::from(std::time::SystemTime::from(obj.published_at()))),
+            description: obj.description().to_owned(),
+            delete: false
+        }
+    }
+}
+
+impl From<Video> for VideoObject {
+    fn from(data: Video) -> Self {
         let cloned = data.video_id.clone();
         InitVideoObject {
             video_id: VideoId::new(data.video_id.clone()),
@@ -146,6 +233,22 @@ impl From<Live> for VideoObject {
             thumbnail_url: format!("https://img.youtube.com/vi/{}/maxresdefault.jpg", cloned),
             ..Default::default()
         }.build()
+    }
+}
+
+impl From<VideoObject> for Video {
+    fn from(obj: VideoObject) -> Self {
+        Self {
+            video_id: obj.video_id().to_owned().into(),
+            channel_id: obj.channel_id().map(|id| id.to_owned().into()),
+            title: obj.title().to_owned(),
+            description: obj.description().to_owned(),
+            published_at: obj.published_at().map(|at| ::prost_types::Timestamp::from(std::time::SystemTime::from(at))),
+            updated_at: obj.updated_at().map(|at| ::prost_types::Timestamp::from(std::time::SystemTime::from(at))),
+            will_start_at: obj.will_start_at().map(|at| ::prost_types::Timestamp::from(std::time::SystemTime::from(at))),
+            started_at: obj.started_at().map(|at| ::prost_types::Timestamp::from(std::time::SystemTime::from(at))),
+            delete: false
+        }
     }
 }
 
